f3972a6a241cf297a5abbed82d860e856fe6efc9
Luca Giorgino <19236900+lucagiorgino@users.noreply.github.com>
2023-04-05 11:15:44 +0200

    Update files with missing license tag

--- src/client.rs
+++ src/client.rs
@@ -1,3 +1,7 @@
+// Copyright (c) 2023, LINKS Foundation
+// SPDX-License-Identifier: Apache-2.0
+// All Rights Reserved. See LICENSE for license details.
+
 use std::time::{Duration, SystemTime, UNIX_EPOCH, Instant};
 use anyhow::{Context, Ok};
 
